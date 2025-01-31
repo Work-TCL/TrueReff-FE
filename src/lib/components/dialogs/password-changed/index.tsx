29cf176701f201f3f9b748f8072241cca87e3e62
yashpatoliya <yashpatoliya333@gmail.com>
2025-02-01 01:10:25 +0530

    FIX | dialog need not close button so add feature skipClose true then not shoeing close button

--- src/lib/components/dialogs/password-changed/index.tsx
+++ src/lib/components/dialogs/password-changed/index.tsx
@@ -15,6 +15,7 @@ export default function PasswordSuccess() {
     <DialogLayout
       open={Boolean(dialogPath)}
       size="!max-w-[638px] w-full overflow-auto"
+      skipClose={true}
     >
       <div className="p-4 sm:p-10 sm:bg-white sm:rounded-md sm:shadow-sm w-full sm:w-[300px] md:w-[500px] text-center overflow-y-auto">
         <span className="mb-4 inline-flex justify-center items-center rounded-full">
--- src/lib/ui/layout/dialog/index.tsx
+++ src/lib/ui/layout/dialog/index.tsx
@@ -6,6 +6,7 @@ import { IoClose } from "react-icons/io5";
 interface IDialogLayout {
   children: ReactNode;
   open: boolean;
+  skipClose?: boolean;
   size?: string;
   handleClose?: () => void;
   isCustomDesign?: boolean;
@@ -15,6 +16,7 @@ export default function DialogLayout({
   children,
   open,
   size,
+  skipClose,
   ...props
 }: IDialogLayout) {
   useEffect(() => {
@@ -38,20 +40,23 @@ export default function DialogLayout({
               className="block fixed inset-0 bg-black bg-opacity-60 transition-opacity z-1 lg:backdrop-blur-none backdrop-blur-sm"
               aria-hidden="true"
             ></Link>
+
             <div
               className={`max-h-[90vh] pb-10 rounded-lg overflow-hidden ${
                 props.isCustomDesign ? "max-w-screen-xl" : "sm:max-w-[90vh]"
               }`}
             >
-              <div className="flex justify-end sticky top-0 pb-1">
-                <Link href="?">
-                  <IoClose
-                    fontSize={25}
-                    color="#ffffff"
-                    className="text-white"
-                  />
-                </Link>
-              </div>
+              {skipClose ? null : (
+                <div className="flex justify-end sticky top-0 pb-1">
+                  <Link href="?">
+                    <IoClose
+                      fontSize={25}
+                      color="#ffffff"
+                      className="text-white"
+                    />
+                  </Link>
+                </div>
+              )}
               <div
                 className={`bg-white relative rounded-lg text-left transition-all ${
                   props.isCustomDesign ? "max-h-[90vh] " : "h-full"
