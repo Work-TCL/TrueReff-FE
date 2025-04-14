33136ad691587923b9333efbcc78b7fc730a87c7
Kunjesh1550 <kunjeshdonga1550@gmail.com>
2025-04-14 23:25:13 +0530

    userOverView

---
+++ src/app/_components/pages/userOverView/cardComponent.tsx
@@ -0,0 +1,76 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@sohanemon/utils";
+import { Package } from "lucide-react";
+
+export const CradComponent = ({
+  title,
+  value,
+  borderColor,
+  bgColor,
+  icon,
+  titleClassName,
+}: {
+  title: string;
+  value: number | string;
+  borderColor: string;
+  bgColor: string;
+  icon?: React.ReactNode;
+  titleClassName?: string;
+}) => {
+  return (
+    <Card
+      className={`w-full h-[100px] box-border border ${borderColor} ${bgColor} rounded-2xl`}
+    >
+      <CardContent className="p-3 flex flex-col justify -between h-full">
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex justify-between items-start">
+            <p
+              className={cn(
+                "text-font-grey md:text-[14px] lg:text-lg",
+                titleClassName
+              )}
+            >
+              {title}
+            </p>
+            {icon ? icon : <Package className="text-Secondary font-normal " />}
+          </div>
+          <p className="font-bold text-secondary">{value}</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const BrandCreatorCard = ({
+  question,
+  btnText,
+  isCreator,
+}: {
+  question: string;
+  btnText: string;
+  isCreator?: boolean;
+}) => {
+  return (
+    <Card className={`w-full h-[145px] box-border border bg-white rounded-2xl`}>
+      <CardContent className="p-3 flex flex-col justify -between h-full">
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex justify-center items-center">
+            <p
+              className={cn(
+                "text-secondary md:text-[14px] lg:text-base  box-border border border-secondary rounded-[12px] py-[11px] px-[40px]",
+                isCreator && "bg-secondary text-white"
+              )}
+            >
+              {btnText}
+            </p>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <p className="text-font-grey text-sm w-[73%] text-center">
+              {question}
+            </p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ src/app/_components/pages/userOverView/view.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import { translate } from "@/lib/utils/translate";
+import ProfileCompletionCard from "../../components-common/charts/profileComplete";
+import useMediaQuery from "@/lib/hooks/useMediaQuery";
+import MyProducts from "./myPoducts";
+import { BrandCreatorCard, CradComponent } from "./cardComponent";
+import { Package, ShoppingBag, Users } from "lucide-react";
+
+export default function UserOverView() {
+  const lg = useMediaQuery("(min-width: 1024px)");
+
+  return (
+    <div className="flex flex-col gap-4 md:p-6 p-4 w-full">
+      <div className="flex flex-col lg:flex-row w-full md:gap-6 gap-4">
+        <div className="flex flex-col md:gap-6 gap-4 w-full lg:max-w-[60%]">
+          {!lg && <ProfileCompletionCard progress={80} />}
+          <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 rounded-[20px] w-full bg-white p-4">
+            <CradComponent
+              title={translate("My_Products")}
+              value={`$${120}`}
+              bgColor="bg-[#FFEDF2]"
+              titleClassName="text-secondary"
+              borderColor={"border-[#FF4979]"}
+              icon={<Package className="text-Secondary font-normal size-5 " />}
+            />
+            <CradComponent
+              title={translate("My Creators")}
+              value={200}
+              borderColor=""
+              bgColor="bg-white"
+              icon={<Users className="text-primary font-normal size-5" />}
+            />
+            <CradComponent
+              title={translate("My Purchased")}
+              value={200}
+              borderColor=""
+              bgColor="bg-white"
+              icon={
+                <ShoppingBag className="text-primary font-normal size-5 " />
+              }
+            />
+          </div>
+          <MyProducts />
+        </div>
+        <div className="flex gap-4">
+          <BrandCreatorCard
+            question={translate("Do you have an e-commerce site to sell?")}
+            btnText={translate("Become a Brand")}
+          />
+          <BrandCreatorCard
+            question={translate("Do you have a 2000+ followers?")}
+            btnText={translate("Become a Creator")}
+            isCreator={true}
+          />
+        </div>
+        {/* {lg && <ProfileCompletionCard progress={80} />} */}
+      </div>
+    </div>
+  );
+}
