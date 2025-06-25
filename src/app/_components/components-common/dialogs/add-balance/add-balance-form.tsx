77ca27073b7c963b566acc31341543065aa802ba
Akshay <akshay10sangani@gmail.com>
2025-06-25 22:37:00 +0530

    min balance added

--- src/app/_components/components-common/dialogs/add-balance/add-balance-form.tsx
+++ src/app/_components/components-common/dialogs/add-balance/add-balance-form.tsx
@@ -34,7 +34,7 @@ export default function AddBalanceForm({
   const [amount, setAmount] = useState<any>(100);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
-  const minAmount = 100;
+  const minAmount = 1;
   const maxAmount = 500000;
 
   // const handlePayment = async ({amount,razorpayOrderId}:IDeposit) => {  
