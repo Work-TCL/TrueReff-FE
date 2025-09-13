d971db6dabf05901d7bd9c41921ea70da8584873
HirenBhuva <hirenbhuva1722@gmail.com>
2025-09-13 17:09:21 +0530

    import changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,10 +3,15 @@
 // Import the Firebase scripts (must be compat for service worker!)
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
-import { firebaseConfig } from "./notifications/firebaseConfig";
-console.log("Firebase config in SW:", firebaseConfig);
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: "AIzaSyABfVhx4Yd70PlKMAypvu0i2Fsaz_V-9fA",
+  authDomain: "truereff-dec3e.firebaseapp.com",
+  projectId: "truereff-dec3e",
+  storageBucket: "truereff-dec3e.firebasestorage.app",
+  messagingSenderId: "12015132178",
+  appId: "1:12015132178:web:d59b64737c5c1e7fbb02cb",
+  measurementId: "G-8WKYZ55KDG"
+});
 
 // Retrieve firebase messaging
 export const messaging = firebase.messaging();
@@ -14,6 +19,7 @@ export const messaging = firebase.messaging();
 // Handle background messages
 messaging.onBackgroundMessage(function(payload) {
   console.log("Received background message ", payload);
+  console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
   const notificationTitle = payload.notification?.title || "Background Message Title";
   const notificationOptions = {
